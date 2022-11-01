a937e59566ea9b12d76fe277aca848bfd8a86fb5
Maryam_Bahwal <mbahwal0001@stu.kau.edu.sa>
2022-11-01 22:46:32 +0300

    create three GUIs: (home , book , customer) create two classes: (book, customer)

---
+++ src/b8a_group1/BookClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b8a_group1;
+
+/**
+ *
+ * @author PC
+ */
+public class BookClass {
+    String title;
+    String auther;
+    int year;
+    double price;
+    
+    public BookClass( String title, String auther, int year, double price){
+        this.title = title;
+        this.auther = auther;
+        this.year = year;
+        this.price = price;
+    }
+    
+    
+    
+}
---
+++ src/b8a_group1/CustomerClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package b8a_group1;
+
+/**
+ *
+ * @author PC
+ */
+public class CustomerClass {
+    String first_name;
+    String last_name;
+    String phone_number;
+    
+    public CustomerClass( String first_name, String last_name, String phone_number){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.phone_number = phone_number;
+    }
+}
